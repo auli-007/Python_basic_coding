c3890dd4188f0cc31c40c1f0d0322d823b72dfca
Shaikh Auliul Hasan <129356569+auli-007@users.noreply.github.com>
2026-05-12 16:39:39 +0600

    Create 05_operators.py for operator examples
    Add basic arithmetic and logical operators demonstration

---
+++ basics/05_operators.py
@@ -0,0 +1,18 @@
+a, b = 10, 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+
+print(True and False)
+print(True or False)
+print(not True)
