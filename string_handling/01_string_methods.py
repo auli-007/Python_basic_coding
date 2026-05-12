48bc5050665ab5350e05e0837d773145e9293bb0
Shaikh Auliul Hasan <129356569+auli-007@users.noreply.github.com>
2026-05-12 17:35:46 +0600

    Update 01_string_methods.py

--- string_handling/01_string_methods.py
+++ string_handling/01_string_methods.py
@@ -1,9 +1,9 @@
-text = "  Hello, Python!  "
-print(text.strip())
-print(text.lower())
-print(text.upper())
-print(text.replace("Python", "World"))
-print(text.split(","))
-print("hello".capitalize())
-print("hello world".title())
-print("python".startswith("py"))
+text = "  Hello, Python!  " 
+print(text.strip()) # Remove spaces from beginning and end
+print(text.lower()) # Convert all letters to lowercase
+print(text.upper()) # Convert all letters to uppercase
+print(text.replace("Python", "World")) # Replace "Python" with "World"
+print(text.split(",")) # Split the string using comma
+print("hello".capitalize()) # Make first letter uppercase
+print("hello world".title()) # Make first letter of every word uppercase
+print("python".startswith("py")) # Check if string starts with "py",true or false
