c523d467ba85361750064d3c36b4bd0e7daebb2c
Shaikh Auliul Hasan <129356569+auli-007@users.noreply.github.com>
2026-05-12 17:21:36 +0600

    Create 03_set.py

---
+++ data_structures/03_set.py
@@ -0,0 +1,6 @@
+colors = {"red", "green", "blue", "red"} 
+print(colors) 
+colors.add("yellow") # Add "yellow" to the set
+colors.discard("green") # Remove "green" from the set
+print("red" in colors) 
+print(colors) 
