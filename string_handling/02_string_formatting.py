92b5e781e4acd0e363d951513c70b821bac58aa5
Shaikh Auliul Hasan <129356569+auli-007@users.noreply.github.com>
2026-05-12 17:30:39 +0600

    Create 02_string_formatting.py

---
+++ string_handling/02_string_formatting.py
@@ -0,0 +1,7 @@
+name = "Auli"
+age = 20
+
+print("My name is " + name)
+print("My name is %s and age is %d" % (name, age))
+print("My name is {} and age is {}".format(name, age))
+print(f"My name is {name} and age is {age}")
