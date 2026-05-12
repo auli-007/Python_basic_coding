b9c31a68464246e758c10a7737b01f77cc6504bc
Shaikh Auliul Hasan <129356569+auli-007@users.noreply.github.com>
2026-05-12 17:20:20 +0600

    Create 02_tuple.py

---
+++ data_structures/02_tuple.py
@@ -0,0 +1,10 @@
+point = (10, 20, 30)   # this is tuple not list
+
+print(point)   # Print the full tuple
+
+print(point[1])   # Print the value at index 1
+
+print("total item:",len(point))   # len() counting all item in tuple
+
+for p in point:   
+    print(p)   
